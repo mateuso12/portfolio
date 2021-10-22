5eb4d012262ee92b61b8543a55309205a7325d06
mateuso12 <mateuso12@hotmail.com.br>
2021-10-22 14:13:57 -0300

    Create theme and global styles

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -5,12 +5,48 @@ export default createGlobalStyle`
     margin: 0;
     padding: 0;
     box-sizing: border-box;
+    font-family: 'Montserrat', sans-serif;
   }
 
   body {
     background: ${props => props.theme.colors.background};
-    color: ${props => props.theme.colors.text};
     font: 400 16px Inter, sans-serif;
+  }
+
+  img { 
+    width: 100%;
+    max-width: 100%;
+  }
+
+  ul { 
+    list-style: none;
+  }
+
+  button {
+    cursor: pointer;
+  }
+
+  a {
+    text-decoration: none;
+  }
+
+  .container {
+    width: 100%;
+    margin: 0 auto;
+    max-width: 85rem;
+    padding: 0 1rem;
+    
+    @media (max-width: 1450px) {
+      max-width: 70rem;
+    }
+
+    @media (max-width: 1000px) {
+      max-width: 50rem;
+    }
+
+    @media (max-width: 700px) {
+      padding: 0 2rem;
+    }
   }
 
 `;
--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,9 +1,15 @@
 const theme = {
-  colors: {
-    background: '#121214',
-    text: '#e1e1e5',
-    primary: '#8257e6'
-  }
+  background: '#11172B',
+  backgroundLight: '#1B2138',
+  primary: '#C9EDDC',
+  secondary: '#89B6A5',
+  gradient: 'linear-gradient(225deg, #313860 0%, #11172B 100%);',
+  text: '#84 8DA0',
+  textHighlight: '#6A706E',
+  textLight: '#EFE6DD',
+  border: '#313958',
+  inputBackground: '#1E253E',
+  error: '#BB4430'
 };
-
 export default theme;
+ 
